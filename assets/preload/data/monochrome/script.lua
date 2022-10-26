06852bffaa2e406f59808d9b4bbceaa5a5f13d84
Quesorraerestu <113881206+Quesorraerestu@users.noreply.github.com>
2022-10-25 22:20:24 -0500

    Add files via upload

---
+++ assets/preload/data/monochrome/script.lua
@@ -0,0 +1,12 @@
+function onSongStart()
+	noteTweenX("NoteMove1", 0, -1000, 0.5, cubeInOut)
+	noteTweenX("NoteMove2", 1, -1000, 0.5, cubeInOut)
+	noteTweenX("NoteMove3", 2, -1000, 0.5, cubeInOut)
+	noteTweenX("NoteMove4", 3, -1000, 0.5, cubeInOut)
+end
+function opponentNoteHit()
+    health = getProperty('health')
+    if getProperty('health') > 0.09 then
+        setProperty('health', health- 0.02);
+    end
+end
